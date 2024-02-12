77a07160d86045c592192fa45d11742ca40b2fd9
grappphy <grappphy@gmail.com>
2024-02-12 13:44:44 +0900

    🔧 chore: add prettier tabWidth 4 config and lint file

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="home"></div>
+    <div class="home"></div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: "HomeView",
-  components: {},
+    name: "HomeView",
+    components: {},
 });
 </script>
