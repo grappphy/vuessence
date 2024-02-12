cb7b41274ed3c8aa2a74c03a7f1f9a1fbc249ec1
grappphy <grappphy@gmail.com>
2024-02-12 18:00:04 +0900

    ✨ feat: el-a11y

---
+++ src/components/elements/a11y/el-a11y.vue
@@ -0,0 +1,25 @@
+<template>
+    <span class="vs-a11y">
+        <slot>
+            {{ text }}
+        </slot>
+    </span>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+    name: "vs-el-a11y",
+    props: {
+        /**
+         * 텍스트
+         *
+         * @type {String}
+         */
+        text: {
+            type: String
+        }
+    }
+});
+</script>
