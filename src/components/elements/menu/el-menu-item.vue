3e700db1ae360b75a4657b85ae6177223364e069
grappphy <grappphy@gmail.com>
2024-02-14 19:02:15 +0900

    ✨ feat: el-menu

---
+++ src/components/elements/menu/el-menu-item.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="vs-menu-item">
+        <slot />
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+    name: "vs-el-menu-item"
+});
+</script>
---
+++ src/components/elements/menu/el-menu.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="vs-menu" :data-vs-placement="placement">
+        <!-- 헤더 -->
+        <slot name="header" :default-class="'vs-menu-header'" />
+
+        <!-- 바디 -->
+        <slot name="body" :default-class="'vs-menu-body'" :list-class="'vs-menu-list'">
+            <div class="vs-menu-body">
+                <!-- 슬롯: 리스트 -->
+                <slot name="list" :default-class="'vs-menu-list'" />
+            </div>
+        </slot>
+
+        <!-- 푸터 -->
+        <slot name="footer" :default-class="'vs-menu-footer'" />
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+    name: "vs-el-menu",
+    props: {
+        /**
+         * 배치
+         *
+         * @type {String}
+         */
+        placement: {
+            type: String
+        }
+    }
+});
+</script>
